8b47c0862f96002f203def79b8ea51008e6e9d83
c12m07 <ximic4@gmail.com>
2022-12-12 16:56:51 -0500

    Config database connection

--- config/database.js
+++ config/database.js
@@ -4,12 +4,12 @@ import * as dotenv from 'dotenv'
 dotenv.config()
 
 const db = new Sequelize(
-  'db_testeo', 
-  'postgres', 
-  'postgres', 
+  'db_a1dm', 
+  'db_a1dm_user', 
+  'G6VwUvTduSeadfwMJeed1ZHZmmFNLroa', 
   
   {
-    host: 'localhost',
+    host: 'dpg-ce0fvuen6mpu84u1srb0-a',
     dialect: 'postgres', 
     logging: false,
     port: '5432', 
