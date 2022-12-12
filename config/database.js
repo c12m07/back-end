70abb91412f03030f361204b8267cb72e5e8394c
c12m07 <ximic4@gmail.com>
2022-12-12 18:00:13 -0500

    Some changes

--- config/database.js
+++ config/database.js
@@ -1,15 +1,19 @@
-import { Sequelize } from 'sequelize';
+import { Sequelize } from "sequelize";
 import * as dotenv from 'dotenv'
+import _config from './config.js'
 
 dotenv.config()
 
+const env = process.env.ENV;
+const config = _config[env];
+
 const db = new Sequelize(
-  process.env.DB_NAME, 
-  process.env.DB_USER, 
-  process.env.DB_PASSWORD, 
+  process.env.DB_NAME || config.database, 
+  process.env.DB_USER || config.username, 
+  process.env.DB_PASSWORD || config.password, 
   
   {
-    host: process.env.DB_HOST,
+    host: process.env.DB_HOST || config.host,
     dialect: 'postgres', 
     logging: false,
     port: '5432', 
